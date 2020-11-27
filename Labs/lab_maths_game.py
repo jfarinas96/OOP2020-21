26e250616aeaedd7a3b865520e5ee3e56c880cbd
Janae Fariñas <C19708735@mytudublin.ie>
2020-11-27 10:36:01 +0000

    Implemented Fibonacci program

--- Labs/lab_maths_game.py
+++ Labs/lab_maths_game.py
@@ -26,7 +26,28 @@ class Fibonacci(MathsGame):
         super().__init__()
 
     def play_game(self):
-        pass
+        how_many = int(input("How many Fibonacci numbers? "))
+
+        # Fibonacci game
+        term1, term2 = 0, 1
+        count = 0
+
+        print("[", end='')
+
+        if how_many <= 0:
+            print("Please enter a number greater than 0")
+        elif how_many == 1:
+            print(term1)
+        else:
+            while count < how_many:
+                print(term1, end='')
+                next_term = term1 + term2
+                term1 = term2
+                term2 = next_term
+                count += 1
+                if count < how_many:
+                    print(", ", end='')
+        print("]")
 
     @property
     def user_input_property(self):
@@ -35,3 +56,6 @@ class Fibonacci(MathsGame):
     @user_input_property.setter
     def user_input_property(self, value):
         self.__user_input = value
+
+f = Fibonacci()
+f.play_game()
