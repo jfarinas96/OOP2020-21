c217d3f41181800878d54a15aa514b9c8a6664ba
Janae Fariñas <C19708735@mytudublin.ie>
2020-11-27 12:14:55 +0000

    Added a while loop to where if user enters 1, the program continues and if user enters 2, the program ends. Validated user inputs

--- Labs/lab_maths_game.py
+++ Labs/lab_maths_game.py
@@ -26,39 +26,74 @@ class Fibonacci(MathsGame):
 
     def __init__(self):
         self.__user_input = 0
+        self.play = 1
         self.counter = 0
-        super().__init__()
 
     def play_game(self):
-        how_many = int(input("How many Fibonacci numbers? "))
+        # Loops until user enters 2 to exit program
+        while True:
+            # Ask user to enter 1 to play, or 2 to exit and checks that user enters an integer that is 1 or 2
+            try:
+                self.play = int(input("Enter 1 to play, or enter 2 to exit: "))
+                if self.play != 1 and self.play != 2:
+                    print("Invalid number - enter 1 or 2\n")
+                    continue
+            except ValueError:
+                print("Invalid input - enter 1 or 2\n")
+                continue
 
-        # Fibonacci game
-        term1, term2 = 0, 1
-        count = 0
+            # Exit program if user enters 2 and displays number of correct answers
+            if self.play == 2:
+                if self.counter == 0:
+                    print("See you next time!")
+                else:
+                    print("You got {0:d} number(s) correct!".format(self.counter))
+                break
 
-        print("[", end='')
+            # Loops until program doesn't come across any errors
+            while True:
+                # Asks for how many terms and checks that user enters an integer
+                try:
+                    how_many = int(input("\nHow many Fibonacci numbers? "))
+                except ValueError:
+                    print("Invalid input - must enter a number")
+                    continue
 
-        if how_many <= 0:
-            print("Please enter a number greater than 0")
-        elif how_many == 1:
-            print(term1)
-        else:
-            while count < how_many:
-                print(term1, end='')
-                next_term = term1 + term2
-                term1 = term2
-                term2 = next_term
-                count += 1
-                if count < how_many:
-                    print(", ", end='')
-        print("]")
+                # Fibonacci game
+                term1, term2 = 0, 1
+                count = 0
 
-        self.user_input_property = int(input("What is the next term? "))
+                # Checks that user enters a number greater than 0
+                if how_many <= 0:
+                    print("Please enter a number greater than 0")
+                    continue
+                else:
+                    print("[", end='')
+                    while count < how_many:
+                        print(term1, end='')
+                        next_term = term1 + term2
+                        term1 = term2
+                        term2 = next_term
+                        count += 1
+                        if count < how_many:
+                            print(", ", end='')
+                    print("]")
 
-        if self.user_input_property <= term1:
-            self.counter += 1
-        else:
-            print("Incorrect - the next term is {0:d}".format(term1))
+                # Asks user for next term and checks that user enters an integer
+                try:
+                    self.user_input_property = int(input("What is the next term? "))
+                except ValueError:
+                    print("Invalid input - must enter a number")
+                    continue
+
+                # Check if number entered is correct and increment counter
+                if self.user_input_property == term1:
+                    print("Correct!\n")
+                    self.counter += 1
+                else:
+                    print("Incorrect - the next term is {0:d}\n".format(term1))
+
+                break
 
     @property
     def user_input_property(self):
