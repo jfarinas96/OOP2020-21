e2ea5eb35adb25c0910bd14df05ff641e4f02ff4
Janae Fariñas <C19708735@mytudublin.ie>
2020-12-04 11:06:50 +0000

    Lab 10 completed

--- Labs/lab_maths_game.py
+++ Labs/lab_maths_game.py
@@ -26,16 +26,17 @@ class Fibonacci(MathsGame):
 
     def __init__(self):
         self.__user_input = 0
-        self.play = 1
-        self.counter = 0
+        self.__play = 1
+        self.__how_many = 0
+        self.__counter = 0
 
     def play_game(self):
         # Loops until user enters 2 to exit program
         while True:
             # Ask user to enter 1 to play, or 2 to exit and checks that user enters an integer that is 1 or 2
             try:
-                self.play = int(input("Enter 1 to play, or enter 2 to exit: "))
-                if self.play != 1 and self.play != 2:
+                self.play_property = int(input("Enter 1 to play, or enter 2 to exit: "))
+                if self.play_property != 1 and self.play_property != 2:
                     print("Invalid number - enter 1 or 2\n")
                     continue
             except ValueError:
@@ -43,18 +44,18 @@ class Fibonacci(MathsGame):
                 continue
 
             # Exit program if user enters 2 and displays number of correct answers
-            if self.play == 2:
-                if self.counter == 0:
+            if self.play_property == 2:
+                if self.counter_property == 0:
                     print("See you next time!")
                 else:
-                    print("You got {0:d} number(s) correct!".format(self.counter))
+                    print("You got {0:d} number(s) correct!".format(self.counter_property))
                 break
 
             # Loops until program doesn't come across any errors
             while True:
                 # Asks for how many terms and checks that user enters an integer
                 try:
-                    how_many = int(input("\nHow many Fibonacci numbers? "))
+                    self.how_many_property = int(input("\nHow many Fibonacci numbers? "))
                 except ValueError:
                     print("Invalid input - must enter a number")
                     continue
@@ -64,18 +65,18 @@ class Fibonacci(MathsGame):
                 count = 0
 
                 # Checks that user enters a number greater than 0
-                if how_many <= 0:
+                if self.how_many_property <= 0:
                     print("Please enter a number greater than 0")
                     continue
                 else:
                     print("[", end='')
-                    while count < how_many:
+                    while count < self.how_many_property:
                         print(term1, end='')
                         next_term = term1 + term2
                         term1 = term2
                         term2 = next_term
                         count += 1
-                        if count < how_many:
+                        if count < self.how_many_property:
                             print(", ", end='')
                     print("]")
 
@@ -89,7 +90,7 @@ class Fibonacci(MathsGame):
                 # Check if number entered is correct and increment counter
                 if self.user_input_property == term1:
                     print("Correct!\n")
-                    self.counter += 1
+                    self.counter_property += 1
                 else:
                     print("Incorrect - the next term is {0:d}\n".format(term1))
 
@@ -102,6 +103,33 @@ class Fibonacci(MathsGame):
     @user_input_property.setter
     def user_input_property(self, value):
         self.__user_input = value
+
+    @property
+    def play_property(self):
+        return self.__play
+
+    @play_property.setter
+    def play_property(self, value):
+        self.__play = value
+
+    @property
+    def how_many_property(self):
+        return self.__how_many
+
+    @how_many_property.setter
+    def how_many_property(self, value):
+        self.__how_many = value
+
+    @property
+    def counter_property(self):
+        return self.__counter
+
+    @counter_property.setter
+    def counter_property(self, value):
+        if value > self.__counter + 1:
+            print("Cannot increment by more than 1 at a time")
+        else:
+            self.__counter = value
 
 
 f = Fibonacci()
